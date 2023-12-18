fc779c862bc5839a1976e1d6a8da119646081779
trung <trung@trung.com>
2023-12-18 21:43:14 +0700

    all other files

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
+using gcsharpRPC.Helpers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<TrungContext>();
 
 var app = builder.Build();
 
