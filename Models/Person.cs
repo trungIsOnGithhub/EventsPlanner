020f8dab94779bc19bf8e3c6afcd01c11cbfa42d
trung <trung@trung.com>
2023-12-18 21:42:30 +0700

    update models and add poll models

--- Models/Person.cs
+++ Models/Person.cs
@@ -1,9 +1,21 @@
-namespace gcsharpRPC.Person
+namespace gcsharpRPC.Models
 {
     public class Person
     {
+        private string name;
+
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name {
+            get {
+                if (string.IsNullOrEmpty(name))
+                    return "";
+
+                return name;
+            }
+            set {
+                name = value;
+            }
+        }
         public int BirthYear { get; set; }
     }
 }
---
+++ Models/Poll.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace gcsharpRPC.Models;
+
+public class Poll
+{
+    public int Id { get; set; }
+    [Required]
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Location { get; set; }
+    public DateTime? CloseDate { get; set; }
+    public Guid PollGuid { get; set; }
+}
