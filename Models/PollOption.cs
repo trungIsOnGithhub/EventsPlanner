09688cbadbce287df46c501e5a68f3e259b398f0
trung-nguyen-v-alephon <trung.nguyen@alephon.com>
2024-09-23 14:15:34 +0700

    fix poll option model

--- Models/PollOption.cs
+++ Models/PollOption.cs
@@ -6,10 +6,15 @@ namespace gcsharpRPC.Models
 
         public DateTime Date { get; set; }
 
-        public string StartTime { get; set; }
+        public float StartTime { get; set; }
 
-        public string EndTime { get; set; }
+        public float EndTime { get; set; }
 
         public ICollection<UserVote> UserVotes { get; set; }  = new List<UserVote>();
+
+        public override string ToString()
+        {
+            return $"On {Date}: from {StartTime} to {EndTime}";
+        }
     }
 }
